9a2339379ac2bab6087fbb77d63c91c41d1b489b
Simfort <lemondzavadavid265@gmail.com>
2025-06-06 15:27:02 +0300

    correct info history

--- src/components/InfoHistory.tsx
+++ src/components/InfoHistory.tsx
@@ -7,34 +7,41 @@ export default function InfoHistory() {
   }, []);
   return (
     <section className="flex items-center flex-col pb-[100px] gap-[50px]">
-      <section className="flex gap-[105px] pt-[50px] pb-[50px] max-lg:flex-col">
-        <div className="flex w-[269px] flex-col">
+      <section className="flex gap-[105px] max-lg:items-center pt-[50px] pb-[50px] max-lg:flex-col">
+        <div className="flex w-[269px]  max-lg:gap-[29px] flex-col max-lg:w-[151px] max-lg:h-[100px] max-lg:items-center">
           <span className="w-[103px] border-[2px] border-[#2F78AE]"></span>
-          <h3 className="text-[32px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
-            с 2008-го года
-          </h3>
-          <p className="text-[16px] font-[Arial] leading-[21px] font-[400] ">
-            группа компаний работает на рынке
-          </p>
+          <div className="text-center">
+            <h3 className="text-[32px] max-md:text-[21px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
+              с 2008-го года
+            </h3>
+            <p className="text-[16px] max-lg:w-[160px] max-lg:h-[42px] font-[Arial] leading-[21px] font-[400] ">
+              группа компаний работает на рынке
+            </p>
+          </div>
         </div>
-        <div className="flex  w-[269px] flex-col">
+        <div className="flex  w-[269px] max-lg:gap-[29px]  flex-col max-lg:w-[219px] max-lg:h-[142px] max-lg:items-center">
           <span className="w-[139px] border-[2px] border-[#2F78AE]"></span>
-          <h3 className="text-[32px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
-            12 лет
-          </h3>
-          <p className="text-[16px] font-[Arial] leading-[21px] font-[400] ">
-            опыт исполнения контрактов 44фз и 223фз на территории Пермского
-            края, Крыма и Севастополя (с 2018 года)
-          </p>
+          <div className="text-center">
+            <h3 className="text-[32px] max-md:text-[21px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
+              12 лет
+            </h3>
+            <p className="text-[16px] font-[Arial] leading-[21px] font-[400] ">
+              опыт исполнения контрактов 44фз и 223фз на территории Пермского
+              края, Крыма и Севастополя (с 2018 года)
+            </p>
+          </div>
         </div>
-        <div className="flex  w-[269px] flex-col">
+        <div className="flex  max-lg:gap-[29px] w-[269px]  flex-col max-lg:w-[234px] max-lg:h-[121px] max-lg:items-center">
           <span className="w-[134px] border-[2px] border-[#2F78AE]"></span>
-          <h3 className="text-[32px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
-            Более 1,5 млрд рублей
-          </h3>
-          <p className="text-[16px] font-[Arial] leading-[21px] font-[400] ">
-            — сумма исполненных обязательств по госконтрактам за последние годы
-          </p>
+          <div className="text-center">
+            <h3 className="text-[32px]  max-md:text-[21px] text-[#2F78AE] leading-[35px] uppercase font-[500]">
+              Более 1,5 млрд рублей
+            </h3>
+            <p className="text-[16px] font-[Arial] leading-[21px] font-[400] ">
+              — сумма исполненных обязательств по госконтрактам за последние
+              годы
+            </p>
+          </div>
         </div>
       </section>
       {winWidth >= 1000 && (
